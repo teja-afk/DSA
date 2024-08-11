b9353f7506b802aba737d0b7e17fcf87da592606
Teja Poosa <145468092+teja-afk@users.noreply.github.com>
2024-08-11 13:33:11 +0530

    Till Recursion class 3
    Till Recursion class 3

---
+++ Basic Math & Pointer/lec-1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int slowExponentiation(int a, int b){
+  int ans = 1;
+  for(int i=0; i<b; i++){
+    ans *= a;
+  }
+  return ans;
+} // TC: O(b)
+
+int fastExponentiation(int a, int b){
+  int ans = 1;
+  while(b > 0){
+    if(b & 1){
+      //odd
+      ans *= a;
+    }
+    a = a * a;
+    b = b >> 1;
+  }
+  return ans;
+} // TC: O(log b)
+
+int main(){
+
+  // cout << slowExponentiation(5, 4) << endl;
+  cout << fastExponentiation(5, 4) << endl;
+
+  return 0;
+}
